a8ae9e74f5a2b640942bdbeeb8b3e8ead42848c6
CampusUser <campususer@csufmac-009.local>
2017-02-27 21:00:07 -0800

    pre bar

--- mvcex/CalculatorViewController.swift
+++ mvcex/CalculatorViewController.swift
@@ -14,6 +14,13 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //MARK: Make swipe gestures
+        let swipeLeft:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target:self, action: #selector(handleSwipe))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        view.addGestureRecognizer(swipeLeft)
+        //handleswipe is a function below
+        //#selector() selects and executes that function
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +28,10 @@ class CalculatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//swipe function
+    func handleSwipe(_ sender:UIGestureRecognizer)
+    {
+        
     }
-    */
 
 }
